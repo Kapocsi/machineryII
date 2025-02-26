5562e45b6c670c393d7d141acfe58957791f0246
Sylvie Mills <smill841@mtroyal.ca>
2025-02-25 20:30:59 -0700

    Model Files Update

--- model.c
+++ model.c
@@ -1,8 +1,26 @@
 #include "model.h"
+#include "global.h"
+
+/**
+ * @brief The minimum permitted Swimmer y-coordinate.
+ */
+static const u32 SW_MIN_Y = 100;
+
+ /**
+  * @brief The subtraction from the swimmer's y-coordinate 
+  * when they bob up.
+  */
+static const u8 SW_SPEED = 10;
+ 
+ /**
+  * @brief The number of characters in the typing row
+  */
+static const u8 ROW_W = 15;
+
 
 void bob_up (Swimmer *swimmer) {
     if (swimmer->y > SW_MIN_Y + SW_SPEED) {
-        swimmer->y -= 10;
+        swimmer->y -= SW_SPEED;
     } else {
         swimmer->y = SW_MIN_Y;
     }
@@ -20,4 +38,24 @@ void switch_sprite (Swimmer *swimmer) {
     }
 }
 
-void
+void set_char (Character *character, int char_index) {
+    character->sprite = char_index;
+}
+
+void shift_pointer (Pointer *pointer) {
+    if (pointer->char_pos < ROW_W - 1) {
+        (pointer->char_pos)++;
+        pointer->x += pointer->length;
+    } else {
+        pointer->char_pos = 0;
+        pointer->x = pointer->x_start;
+    }
+}
+
+void increase_counter (Counter *counter) {
+    /*This function needs to check if the counter is maxed out,
+    and also set the sprites of the counters*/
+    TODO()
+    (counter->value)++;
+}
+
--- model.h
+++ model.h
@@ -10,27 +10,6 @@
 
 #include "bitmap.h"
 
-
-/**
- * @brief The minimum permitted Swimmer y-coordinate.
- */
-#define SW_MIN_Y = 100; 
-
-/**
- * @brief The change in the swimmer's y-coordinate when they bob up.
- */
-#define SW_SPEED = -10; 
-
-/**
- * @brief The minimum permitted Pointer x-coordinate.
- */
-#define PT_MIN_X = 60; 
-
-/**
- * @brief The maximum permitted Pointer x-coordinate.
- */
-#define PT_MAX_X = 580; 
-
 /**
  * @struct Swimmer
  * @brief Represents the player character.
@@ -50,34 +29,71 @@
 
 
 /**
- * @struct Letter
- * @brief Represents a stationary letter slot.
+ * @struct Character
+ * @brief Represents a stationary character slot.
  *
- * [FILL THIS IN].
+ * Displays a single character value.
  */
  typedef struct {
-    const u16 x;    /* The x-coordinate of the left edge of the letter's sprite
+    const u16 x;    /* The x-coordinate of the left edge of the character's sprite
                         on the screen*/
-    const u16 y;    /* The y-coordinate of the top edge of the letter's sprite
+    const u16 y;    /* The y-coordinate of the top edge of the character's sprite
                         on the screen*/
-    u8 sprite;      /* The index of the letter's current sprite based on the
+    u8 sprite;      /* The index of the character's current sprite based on the
                         sprite array*/
-    BitMap sprites[];   /* The array of all of the letter's sprites*/
-} Letter;
+    BitMap sprites[37];   /* The array of all of the character's sprites*/
+} Character;
 
 
 /**
  * @struct Pointer
  * @brief A horizontal line indicating the next letter to be typed.
  *
- * [FILL THIS IN].
+ * When the player presses the indicated letter, the pointer will shift to the
+ * next letter.
  */
  typedef struct {
+    u16 x_start;    /* The x-coordinate of the left side of the line at
+                        position 0*/
     u16 x;          /* The x-coordinate of the left side of the line*/
     const u16 y;    /* The y-coordinate of the line*/
     const u8 length; /* The length of the line in pixels*/
+    u8 char_pos;    /* Which character the pointer is positioned under
+                        in the row - left to right, 0 to (ROW_W - 1)*/
 } Pointer;
 
 
+/**
+ * @struct Counter
+ * @brief A string of numbers representing the player's point total.
+ */
+ typedef struct {
+    const u16 x;    /* The x-coordinate of the left edge of the leftmost
+                        character sprite*/
+    const u16 y;    /* The y-coordinate of the top edge of the character
+                        sprites*/
+    u16 value;      /* The point value represented by the counter*/                    
+    Character numbers[4];   /* The array of all of the letter's sprites*/
+} Counter;
+
+
+/* Decreases the swimmer's y value by a set amount */
+void bob_up (Swimmer *swimmer);
+
+/* Increases the swimmer's y value by a set amount */
+void sink (Swimmer *swimmer);
+
+/* Switches the swimmer's current sprite */
+void switch_sprite (Swimmer *swimmer);
+
+/* Sets the sprite of the character */
+void set_char (Character *character, int char_index);
+
+/* Increases the pointer's x value, or returns it to its starting position */
+void shift_pointer (Pointer *pointer);
+
+/* Increases the point value of the counter, and changes its appearance to 
+   reflect the new value */
+void increase_counter (Counter *counter);
 
  #endif
