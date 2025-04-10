b84ce6d462c7a11a79c95c12fe18478c2a1eef30
Thomas Kapocsi <thomas@kapocsi.ca>
2025-04-10 10:30:31 -0600

    Changes

--- src/is_super.s
+++ src/is_super.s
@@ -1,11 +1,10 @@
-        xdef    _is_supervisor
-_is_supervisor:
+        xdef    _is_super
+_is_super:
         move    sr, d0         ; Move status register to D0
         andi.w  #$2000, d0     ; Mask out the supervisor bit (bit 13)
         beq     not_supervisor ; If zero, not in supervisor mode
         moveq   #1, d0         ; Supervisor mode
         rts
-
-not_supervisor:
+not_super:
         moveq   #0, d0         ; Not in supervisor mode
         rts
